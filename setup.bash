28af2e039377d71bb541e0c9b61cafff2003815c
Joey Angeja <angeja16@gmail.com>
2018-03-21 16:20:48 -0700

    setup.bash

---
+++ setup.bash
@@ -0,0 +1,6 @@
+git clone https://github.com/lz4/lz4.git lz4
+git clone https://github.com/andikleen/snappy-c.git snappy
+git clone https://github.com/open-mpi/ompi.git mpi
+git clone https://github.com/richgel999/miniz.git miniz
+
+mkdir mpiBuild
